faff6fb65de02e378daba8cf6d8128898cc61a73
Anita <dolar2007@ukr.net>
2023-06-02 18:47:24 +0300

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const imagesMarkup = createGallery(galleryItems);
+
+galleryEl.insertAdjacentHTML("afterbegin", imagesMarkup);
+
+function createGallery (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" 
+                href="${original}">
+            <img class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" />
+        </a>
+     </li>`
+    })
+    .join(" ")
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+     /* options */ 
+     captionsData: "alt",
+     captionDelay: 250,
+    });
